ee8a284ea1d094e349fa999cddd9ef25598fa7fe
Tony Ho <ho.tonym@gmail.com>
2018-05-16 15:48:16 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,32 @@
+require "pry"
 
+class CashRegister
+
+attr_accessor :discount, :total
+attr_writer
+attr_reader
+
+  def initialize(d_amount = 20)
+    @discount = d_amount
+    @total = 0
+  end
+
+  # def add_item(string, total)
+  #   @total = total
+  # end
+   def add_item(string_item, total, quantity = 1)
+     @total = @total + (total * quantity)
+   end
+
+   def apply_discount
+     d = @discount/100.0
+     discount_amount = 1 - d # to ge the % amount we want to multiply by
+     @total = (@total * discount_amount).to_nearest_i
+
+     if @discount == 0 || @discount == nil
+        "There is no discount to apply."
+     else
+      "After the discount, the total comes to #{@total}."
+    end
+   end
+end
